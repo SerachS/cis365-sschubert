247ee9b23453f1b0b852e1490c25dbaca765a060
Serach Schubert <serachschubert@gmail.com>
2024-02-28 19:09:20 -0500

    sschubert

---
+++ Class 6A/Classwork/js/lab08-test01.js
@@ -0,0 +1,17 @@
+/* add code below this */
+let bill =prompt("what is the bill total?");
+let ibill=Number(bill);
+
+const TIP_PRECENTAGE= 0.10;
+let tip = ibill * TIP_PRECENTAGE;
+
+
+
+if (ibill && ibill != 0)
+{
+    console.log("For bill of $" + bill + " the tip should be $" + tip);
+    //console.log(`For bill of $${bill} the tip should be $${tip}`);
+}
+else{
+    console.log(`${bill} is not a valid number`);
+}
