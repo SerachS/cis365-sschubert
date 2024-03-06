49e8febcc0ac0522e8311a66fb1faaf84a374eaf
Serach Schubert <serachschubert@gmail.com>
2024-03-05 19:53:55 -0500

    sschubert

--- Class 6B/Classwork/js/lab08-test04.js
+++ Class 6B/Classwork/js/lab08-test04.js
@@ -1,49 +1,70 @@
 
 // convert comma-separated values into an array
 
-
+let countries = csv.split(',');
+console.log(countries);
 // convert array into delimited string
-
+let joinedCountries = countries.join();
+console.log(joinedCountries);
 
 // check if these are arrays
-
+console.log(Array.isArray(countries));
+console.log(Array.isArray(joinedCountries));
 
 //  sort a simple array 
-
+countries.sort()
+console.log(countries);
 
 // reverse the sort
-
+countries.reverse()
+console.log(countries);
 
 // remove the first element
-
+countries.shift();
+console.log(countries);
 
 // remove the last element
 
+countries.pop();
+console.log(countries);
 
 // add new elements to the front of the array
 
-
+countries.unshift("Israel","Japan");
+console.log(countries);
 // search for element
 
-
+console.log(countries.includes("Germany"));
 // search for element index
-
+console.log(countries.indexOf("Germany"));
 
 // make a new array by extracting from another array
+let newCountries = countries.splice(0,6);
 
-
-console.log('---------------------');
+console.log(newCountries);
 
 // there are other array methods which you will learn in Ch 10 */
 
 
 // use a loop to output all cities whose continent=="NA"
-
+for (let city of cities)
+{
+   if (city.continent == "NA")
+   {
+      console.log(city);
+   }
+}
 
 console.log('---------------------');
 
 // use a loop to output gallery names whose country=="USA"
-
+for (let gallery of galleries)
+{
+   if (gallery.location.country == "USA")
+   {
+      console.log(gallery.name);
+   }
+}
 
 console.log('---------------------');
 
@@ -51,7 +72,13 @@ console.log('---------------------');
 const colors = JSON.parse(colorsAsString);
 
 // use a loop to output color name if luminance < 75
-
+for (let color of colors)
+{
+   if (color.luminance < 75)
+   {
+      console.log(color.name);
+   }
+}
 
 console.log('---------------------');
 
