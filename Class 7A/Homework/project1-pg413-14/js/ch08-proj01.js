5e95c5e961058b530a5978cbd08eb660153a4df5
Serach Schubert <serachschubert@gmail.com>
2024-03-12 17:34:31 -0400

    sschubert

--- Class 7A/Homework/project1-pg413-14/js/ch08-proj01.js
+++ Class 7A/Homework/project1-pg413-14/js/ch08-proj01.js
@@ -4,3 +4,46 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+let subtotal = 0;
+let shipping = 0;
+   for (let row of cart)
+{
+   document.write('<tr>');
+   outputCartRow(row, row.quantity);
+   document.write('</tr>');
+   subtotal+= calculateTotal(row.quantity,row.product.price);
+}
+if (subtotal > shipping_threshold)
+{
+   shipping = 0;
+}
+else
+{
+   shipping = 40;
+}
+
+let tax = subtotal * tax_rate;
+let grandTotal = subtotal + shipping + tax;
+
+document.write('<tr class = "totals">');
+document.write('<td colspan = "4">' + "Subtotal: " + '<td/');
+document.write('<td>' + "$" + subtotal.toFixed(2) + '</td>');
+document.write('</tr>');
+
+document.write('<tr class = "totals">');
+document.write('<td colspan = "4"> ' + "Tax: " + '<td/');
+document.write('<td>'+ "$" + tax.toFixed(2) + '</td>');
+document.write('</tr>');
+
+document.write('<tr class = "totals">');
+document.write('<td colspan = "4"> ' + "Shipping: " + '<td/');
+document.write('<td>'+ "$" + shipping.toFixed(2) + '</td>');
+document.write('</tr>');
+
+document.write('<tr class = "totals">');
+document.write('<td colspan = "4" class="focus"> ' + "Grand Total: " + '<td/');
+document.write('<td class="focus">'+ "$" + grandTotal.toFixed(2) + '</td>');
+document.write('</tr>');
+
+
+
--- Class 7A/Homework/project1-pg413-14/js/functions.js
+++ Class 7A/Homework/project1-pg413-14/js/functions.js
@@ -1,4 +1,21 @@
 /* define your functions here */
+function calculateTotal(quantity, price)
+{
+    return (quantity*price);
+}
+function outputCartRow(item = "Portrait of Marten Soolmans", total) 
+{
+
+    let picture = item.product.filename;
+    document.write('<td><img src = images/' + picture + ' ></td>');
+    document.write('<td>' + item.product.title + '</td>');
+    document.write('<td>' + total + '</td>');
+    price = (item.product.price).toFixed(2);
+    document.write('<td>' + price + '</td>');
+    calcTotal = (calculateTotal(total,item.product.price)).toFixed(2);
+    document.write('<td>' + calcTotal + '</td>');
+}
+
 
 
 
