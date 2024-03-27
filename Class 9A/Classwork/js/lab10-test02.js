4fdb7648a54020a8d3f95c823685c1f892fadbf1
Serach Schubert <serachschubert@gmail.com>
2024-03-26 21:23:41 -0400

    sschubert

---
+++ Class 9A/Classwork/js/lab10-test02.js
@@ -0,0 +1,15 @@
+// import the module here
+import {getSampleGalleries} from "./gallery";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+   const galleries = getSampleGalleries();
+
+   // you'll be adding code to this <ul> list
+   const list = document.querySelector("#galleryList");
+    
+   // now loop through the sample gallery data calling render and list.appendChild
+
+    
+
+});
